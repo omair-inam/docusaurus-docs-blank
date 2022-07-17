5e52703e20cbbeac950bb822998344fc10553d5b
Omair-Inam Abdul-Matin <omairinam.abdulmatin@splashthat.com>
2022-07-17 23:35:19 +0300

    Set Github user placeholder to UpdateMe_GithubUser

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'UpdateMe', // Usually your GitHub org/user name.
+  organizationName: 'UpdateMe_GithubUser', // Usually your GitHub org/user name.
   projectName: 'UpdateMe', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
